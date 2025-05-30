459c5b252326099934775c3a70abf7abd7a508a0
mellowmac <elbysoniq@gmail.com.noreply.github.com>
2025-05-30 23:49:23 +0200

    modified:   12_calculator/calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+    return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    const array = [];
+
+
+    const sumTotal = array.reduce(
+        (accumulator, currentValue) => accumulator + currentValue);
+
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+    return a * b
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return Math.pow(a, b)
 };
 
-const factorial = function() {
+const factorial = function(a) {
+    let res = 1;
+    for (let i = 1; i <= a; i++){
+         res *= i;
+    }
+    
+    return res
+
 	
 };
 
