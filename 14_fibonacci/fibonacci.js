ae7049080d55c6982813e359b9718747cf53d3ca
mellowmac <elbysoniq@gmail.com.noreply.github.com>
2025-05-31 19:42:58 +0200

    modified:   14_fibonacci/fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+function fibonacci(num) {
+   let sum;
+   let num1 = 0;
+   let num2 = 1;
 
-};
+   if (num === 0) {
+        num1 = 0;
+        num2 = 0;
+   } else if (num === 1) {
+        num2 = 1;
+}
+    for (let i = 1; i < num; i++){
+        sum = num1 + num2;
+            console.log(sum);
+        num1 = num2;
+            console.log(num1);
+        num2 = sum;
+            console.log(num2);
+
+    }
+     return num2
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
